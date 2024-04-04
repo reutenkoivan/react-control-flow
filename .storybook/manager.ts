f03b9e1169f28369e9b07c998405f4bd64972d01
Ivan Reutenko <37533785+reutenkoivan@users.noreply.github.com>
2024-04-04 11:49:07 +0300

    feat(#1): Added the fallback prop for the For component. (#2)

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -5,6 +5,7 @@ import packageJson from '../package.json' assert { type: 'json' }
 const theme = create({
 	base: 'light',
 	brandTitle: packageJson.name,
+  // TODO: use the PUBLIC_URL env variable
 	brandImage: process.env.NODE_ENV === 'production' ? '/react-control-flow/banner.png' : '/banner.png',
 })
 
